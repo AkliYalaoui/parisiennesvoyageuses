bd4c21a979ba5b79326c14799aa2e618712f3f24
AkliYalaoui <akliyalaoui@gmail.com>
2025-02-25 02:55:58 +0100

    Redesign About page

--- app/[locale]/(content)/about/page.js
+++ app/[locale]/(content)/about/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { FaInstagramSquare } from "react-icons/fa";
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
+import { Pacifico } from "next/font/google";
+
+const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
   title: "About",
@@ -74,18 +77,19 @@ const AboutPage = () => {
   ];
 
   return (
-    <section className="bg-gradient-to-b from-orange-50 to-white">
+    <section className=" md:pt-20">
       <div className="container mx-auto px-6 md:px-12">
         {/* Introduction */}
-        <div className="text-center mb-12">
-          <h1 className="text-4xl font-bold text-slate-950">{t("title")}</h1>
-          <p className="text-lg text-gray-600 mt-4">{t("subTitle")}</p>
-        </div>
+        <h1
+          className={` text-coffee text-4xl sm:text-5xl lg:text-6xl font-extrabold  mb-14 ${pacifico.className}`}
+        >
+          {t("title")}
+        </h1>
 
         {/* Our Story */}
         <div className="flex flex-col md:flex-row justify-between items-center gap-8">
           <div className="flex-1">
-            <h2 className="text-3xl font-semibold text-slate-950 mb-4">
+            <h2 className="text-2xl font-semibold text-warmbrown mb-4">
               {t("meet")}
             </h2>
             <p className="text-lg text-gray-700 mb-4">{t("p1")}</p>
@@ -95,9 +99,9 @@ const AboutPage = () => {
               href="https://www.instagram.com/yourpage"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center p-2 bg-slate-900 text-white font-bold text-lg rounded-lg shadow-lg hover:bg-slate-950 transition-all duration-300"
+              className="inline-flex items-center p-2 bg-rose-500 text-peach rounded-lg hover:bg-softpink transition-all duration-300"
             >
-              <FaInstagramSquare className="mr-3 text-2xl" />
+              <FaInstagramSquare className="mr-3 text-xl" />
               {t("cta2")}
             </Link>
           </div>
@@ -107,23 +111,23 @@ const AboutPage = () => {
             <Image
               src="/twins.png"
               alt="Twin Sisters"
-              width={500}
-              height={500}
-              className="rounded-lg shadow-lg"
+              width={400}
+              height={400}
+              className="rounded-lg shadow"
             />
           </div>
         </div>
 
         {/* Our Travel Adventures */}
         <div className="mt-16">
-          <h2 className="text-3xl font-semibold text-slate-950 text-center mb-8">
+          <h2 className="text-2xl font-semibold text-warmbrown mb-8">
             {t("title2")}
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {adventures.map((adventure) => (
               <div
                 key={adventure.city}
-                className="bg-white rounded-lg shadow-lg overflow-hidden"
+                className="bg-peach rounded-lg shadow-lg overflow-hidden"
               >
                 <Image
                   src={`/travel/${adventure.city.replace(" ", "")}.jpg`}
@@ -133,10 +137,10 @@ const AboutPage = () => {
                   className="object-cover w-full h-64"
                 />
                 <div className="p-6">
-                  <h3 className="text-xl font-semibold text-slate-950 mb-2">
+                  <h3 className="text-xl text-center font-semibold text-coffee mb-2">
                     {adventure.title}
                   </h3>
-                  <p className="text-gray-700">{adventure.description}</p>
+                  <p className="text-warmbrown">{adventure.description}</p>
                 </div>
               </div>
             ))}
@@ -144,14 +148,14 @@ const AboutPage = () => {
         </div>
 
         {/* Why We Started */}
-        <div className="mt-16">
-          <h2 className="text-3xl font-semibold text-slate-950 text-center mb-8">
+        <div className="mt-16 bg-softblue p-4">
+          <h2 className="text-2xl font-semibold text-coffee text-center mb-8">
             {t("title3")}
           </h2>
-          <p className="text-lg text-gray-700 text-center max-w-4xl mx-auto">
+          <p className="text-lg text-warmbrown text-center max-w-4xl mx-auto">
             {t("p4")}
           </p>
-          <p className="text-lg text-gray-700 text-center mt-4 max-w-4xl mx-auto">
+          <p className="text-lg text-warmbrown text-center mt-4 max-w-4xl mx-auto">
             {t("p5")}
           </p>
         </div>
@@ -160,7 +164,7 @@ const AboutPage = () => {
         <div className="mt-10 text-center">
           <Link
             href="/guide"
-            className="p-2 bg-slate-950 text-white font-semibold rounded-lg hover:bg-slate-900 transition"
+            className="p-2 bg-coffee text-peach font-semibold rounded-lg hover:bg-warmbrown transition"
           >
             {t("cta2")}
           </Link>
